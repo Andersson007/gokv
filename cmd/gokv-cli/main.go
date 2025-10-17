efd9356104703b76984f853e2cf1d7fb5221096b
Andrew Klychkov <andrew.a.klychkov@gmail.com>
2025-10-17 08:31:09 +0200

    gokv-cli: reading input in loop

--- cmd/gokv-cli/main.go
+++ cmd/gokv-cli/main.go
@@ -1,8 +1,44 @@
 // Command gokv-cli: Main client entry point
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("GoKV Client")
+	clientVer := "0.1.0"  // TODO More idiomatic way?
+	// Some defaults here
+	// TODO Overwrite them by using CLI args
+	// port :=
+	// proto :=
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("GoKV client ver.", clientVer)
+	fmt.Println("Enter commands (type 'exit' or press Ctrl+D to quit):")
+
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {	// Read next line
+			break				// EOF (including Ctrl+D) or error
+		}
+
+		input := scanner.Text()
+
+		if input == "exit" {
+			fmt.Println("Exit")
+			break
+		}
+
+		if input != "" {
+			fmt.Println("You entered:", input)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
 }
