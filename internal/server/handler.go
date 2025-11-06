af39b8c8f827e83188576ab79a1c6cbad837c6ed
Andrew Klychkov <andrew.a.klychkov@gmail.com>
2025-11-06 10:45:07 +0100

    Moved responding to client to a func

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -27,6 +27,57 @@ func (e HandlerError) Error() string {
 	return fmt.Sprintf("[%d] %s", e.Code, e.Msg)
 }
 
+func respondToClient(
+	log chan logger.LogEntry,
+	conn net.Conn,
+	store storage.StorageEnginer,
+	dc protocol.DataCmd,
+	addr string,
+	msg logger.LogEntry,
+) error {
+	switch dc.Cmd {
+	case protocol.GET:
+		val, err := store.Get(dc.Key)
+		if err != nil {
+			if errors.Is(err, storage.ErrKeyNotFound) {
+				conn.Write([]byte("The key doesn't exist: " + dc.Key))
+   			} else {
+				conn.Write([]byte("Error: " + dc.Key))
+   			}
+		} else {
+			// If not errors, send the val to the client
+			conn.Write([]byte(val))
+		}
+	case protocol.SET:
+		store.Set(dc.Key, dc.Val)
+		conn.Write([]byte("SET"))
+	case protocol.DEL:
+		store.Del(dc.Key)
+		conn.Write([]byte("DELETED"))
+	}
+
+	if dc.Cmd == protocol.EXIT {
+		fmt.Println("Connection closed by client", addr)
+		log <- msg.New(logger.INFO,
+			"Connection closed by client", addr)
+
+		// TODO This is discarded now. Think of creating a channel
+		// to return errors from handler goroutines if needed
+		// TODO UPDATE: I know the case. When connection is closed
+		// by the client, server Listen() reports the error
+		// "Error reading from connection: EOF". The solution could
+		// be 1. Creating a channel 2. Assigning ID to each
+		// handler goroutine 3. There'll be a check in the reading
+		// from conn loop checking if the gorouting has reported
+		// that connection was closed by the client.
+		return HandlerError{
+			Code: ErrClientClosedConn,
+			Msg: "connection closed by client",
+		}
+	}
+	return nil
+}
+
 func HandleConn(
 	log chan logger.LogEntry,
 	conn net.Conn,
@@ -50,37 +101,9 @@ func HandleConn(
 
 		dc := protocol.Parse(log, string(buf[:n]))
 
-		// TODO Move to the switch to a function
-		switch dc.Cmd {
-		case protocol.GET:
-			val, err := store.Get(dc.Key)
-			if err != nil {
-				if errors.Is(err, storage.ErrKeyNotFound) {
-					conn.Write([]byte("The key doesn't exist: " + dc.Key))
-    			} else {
-					conn.Write([]byte("Error: " + dc.Key))
-    			}
-			} else {
-				// If not errors, send the val to the client
-				conn.Write([]byte(val))
-			}
-		case protocol.SET:
-			store.Set(dc.Key, dc.Val)
-			conn.Write([]byte("SET"))
-		case protocol.DEL:
-			store.Del(dc.Key)
-			conn.Write([]byte("DELETED"))
-		}
-
-		if dc.Cmd == protocol.EXIT {
-			fmt.Println("Connection closed by client", addr)
-			log <- msg.New(logger.INFO,
-				"Connection closed by client", addr)
-			return HandlerError{
-				Code: ErrClientClosedConn,
-				Msg: "connection closed by client",
-			}
-		}
+		// TODO This function returns an error.
+		// Think if you need it. TODO see comments in the function
+		respondToClient(log, conn, store, dc, addr, msg)
 	}
 	return nil
 }
